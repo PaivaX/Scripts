40eb49754456e9bddc231ff3b51391f127ddf3fd
Rui Paiva <75069807+PaivaX@users.noreply.github.com>
2024-06-27 10:37:37 +0100

    Update install_docker_and_portainer.sh

--- install_docker_and_portainer.sh
+++ install_docker_and_portainer.sh
@@ -38,6 +38,6 @@ echo
 sudo docker volume create portainer_data
 
 # Create container of Portainer
-sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 echo
 echo -e "\e[32mDocker e Portainer  instalados com sucesso! - By Rui Paiva\e[0m"
